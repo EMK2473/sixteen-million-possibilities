e139e43f6c5abe4f876906bf699175ca1ee06fb0
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-02-12 01:25:04 -0500

    confirmed 16 million + possiblities

--- sixteenMillion.js
+++ sixteenMillion.js
@@ -1,6 +1,6 @@
-export const generateRandomHexColor = () => {
+const generateRandomHexColor = () => {
     const letters = "0123456789ABCDEF";
-    const length = Math.floor(Math.random() * 5) + 2; // Random number between 2 and 6
+    const length = Math.floor(Math.random() * 5) + 2; 
     let color = "#";
     for (let i = 0; i < length; i++) {
         color += letters[Math.floor(Math.random() * 16)];
